f6c9c12c887b96adc145774f2438e195e79924b7
Tung Nguyen <tungnvan.mar12th@gmail.com>
2021-03-09 10:10:02 +0700

    Refactor code

--- scripts/index.js
+++ scripts/index.js
@@ -14,62 +14,48 @@ function main() {
       })
       .then((fresponse) => {
         fragmentShaderSource = fresponse.data;
+        
+        // Crate Shaders and link them to a program
         const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
         const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
         const program = createProgram(gl, vertexShader, fragmentShader);
 
-        const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
-        const resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution');
-        const positionBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-        // three 2d points
-        const positions = [
-          0, 0,
-          0, 720,
-          1280, 0,
-          0, 720,
-          1280, 0,
-          1280, 720,
-        ];
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-        // Clear the canvas
-        gl.clearColor(0, 0, 0, 0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
-
-        // Tell it to use our program (pair of shaders)
+        // Currently use this program for GL
         gl.useProgram(program);
 
-        // Next we need to tell WebGL how to take data from the buffer we setup above and supply it to the attribute in the shader. 
-        // First off we need to turn the attribute on
-        gl.enableVertexAttribArray(positionAttributeLocation);
+        function drawTriangle(gl) {
+          // Create and bind buffer to gl global bind point
+          const xyBuffer = gl.createBuffer();
+          gl.bindBuffer(gl.ARRAY_BUFFER, xyBuffer);
 
-        gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+          // Push X-Y coordinates to bound buffer
+          const xyCoords = [
+            randomInteger(0, 1280), randomInteger(0, 720),
+            randomInteger(0, 1280), randomInteger(0, 720),
+            randomInteger(0, 1280), randomInteger(0, 720),
+          ];
+          gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(xyCoords), gl.STATIC_DRAW);
 
-        // Bind the position buffer.
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+          // Get locations of neccessary vars in program
+          const aXyLocation = gl.getAttribLocation(program, 'a_xy');
+          const uProjectionMatrixLocation = gl.getUniformLocation(program, 'u_projection_matrix');
 
-        // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-        const size = 2;          // 2 components per iteration
-        const type = gl.FLOAT;   // the data is 32bit floats
-        const normalize = false; // don't normalize the data
-        const stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-        const offset = 0;        // start at the beginning of the buffer
-        gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
+          render(gl, aXyLocation, uProjectionMatrixLocation, xyBuffer);
 
-        // draw
-        const primitiveType = gl.TRIANGLES;
-        const count = 6;
-        gl.drawArrays(primitiveType, offset, count);
+          setTimeout(() => {window.requestAnimationFrame(() => {drawTriangle(gl)})}, 1000);
+        }
+
+        window.requestAnimationFrame(() => {drawTriangle(gl)});
       })
       .catch((err) => {
         console.error(err);
       });
     }
 };
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function createShader(gl, type, source) {
   var shader = gl.createShader(type);
@@ -94,6 +80,51 @@ function createProgram(gl, vertexShader, fragmentShader) {
   }
   console.log(gl.getProgramInfoLog(program));
   gl.deleteProgram(program);
+}
+
+function render(gl, aXyLocation, uProjectionMatrixLocation, xyBuffer) {
+  const projectionMatrix = new Float32Array([
+    2 / gl.canvas.width, 0,                     0, 0,
+    0,                   2 / gl.canvas.height,  0, 0,
+    0,                   0,                     0, 0,
+    -1,                  -1,                    0, 1,
+  ]);
+
+  // Set viewport width and height
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  // Clear the canvas
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Set value for the projection matrix
+  gl.uniformMatrix4fv(
+    uProjectionMatrixLocation, // Projection matrix uniform location
+    false,                     // Transpose must be false
+    projectionMatrix,          // The projection matrix
+  );
+
+  // Next we need to tell WebGL how to take data from the buffer we setup above and supply it to the attribute in the shader. 
+  // First off we need to turn the attribute on
+  gl.enableVertexAttribArray(aXyLocation);
+
+  // Bind the position buffer.
+  gl.bindBuffer(gl.ARRAY_BUFFER, xyBuffer);
+
+  // Tell the attribute how to get data out of the buffer currently bound to gl.ARRAY_BUFFER (xyBuffer)
+  // After call gl.vertexAttribPointer, the gl.ARRAY_BUFFER is free to bind to another buffer, 
+  // because a_xy attribute has been set to use data from xyBuffer
+  const size = 2;          // 2 components per iteration
+  const type = gl.FLOAT;   // the data is 32bit floats
+  const normalize = false; // don't normalize the data
+  const stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+  const offset = 0;        // start at the beginning of the buffer
+  gl.vertexAttribPointer(aXyLocation, size, type, normalize, stride, offset);
+
+  // draw
+  const primitiveType = gl.TRIANGLES;
+  const count = 3;
+  gl.drawArrays(primitiveType, offset, count);
 }
 
 window.addEventListener('load', main);
